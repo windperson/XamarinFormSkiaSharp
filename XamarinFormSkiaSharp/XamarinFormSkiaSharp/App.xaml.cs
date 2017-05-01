66fc71cee7437cde27276528834a20e6a7f1fb65
Charles Pao <windperson@gmail.com>
2017-05-01 23:51:25 +0800

    Add Visual Studio Mobile Center SDK

--- XamarinFormSkiaSharp/XamarinFormSkiaSharp/App.xaml.cs
+++ XamarinFormSkiaSharp/XamarinFormSkiaSharp/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,6 +22,9 @@ namespace XamarinFormSkiaSharp
 		protected override void OnStart ()
 		{
 			// Handle when your app starts
+			MobileCenter.Start("ios=9ee32471-13e7-496b-9af4-ffd31c4328a8;" +
+				   "android=c898ed81-5c24-41a5-92ef-d620e994a568",
+				   typeof(Analytics), typeof(Crashes));
 		}
 
 		protected override void OnSleep ()
