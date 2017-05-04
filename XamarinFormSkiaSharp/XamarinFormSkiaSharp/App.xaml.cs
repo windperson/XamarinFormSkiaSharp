53f78b34ed12de806ebf9d91a7202d9ce4462d8b
Charles Pao <windperson@gmail.com>
2017-05-04 20:05:10 +0800

    1. Adapt Visual Studio mobile Center code in UWP 2. Make all plateform can print scale in running output log during  debug time.

--- XamarinFormSkiaSharp/XamarinFormSkiaSharp/App.xaml.cs
+++ XamarinFormSkiaSharp/XamarinFormSkiaSharp/App.xaml.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Azure.Mobile;
+﻿#if !WINDOWS_UWP
+using Microsoft.Azure.Mobile;
 using Microsoft.Azure.Mobile.Analytics;
 using Microsoft.Azure.Mobile.Crashes;
+#endif
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,27 +14,30 @@ namespace XamarinFormSkiaSharp
 {
 	public partial class App : Application
 	{
-		public App ()
+		public App()
 		{
 			InitializeComponent();
 
 			MainPage = new XamarinFormSkiaSharp.MainPage();
 		}
 
-		protected override void OnStart ()
+		protected override void OnStart()
 		{
+#if !WINDOWS_UWP
 			// Handle when your app starts
 			MobileCenter.Start("ios=9ee32471-13e7-496b-9af4-ffd31c4328a8;" +
 				   "android=c898ed81-5c24-41a5-92ef-d620e994a568",
 				   typeof(Analytics), typeof(Crashes));
+#endif
+
 		}
 
-		protected override void OnSleep ()
+		protected override void OnSleep()
 		{
 			// Handle when your app sleeps
 		}
 
-		protected override void OnResume ()
+		protected override void OnResume()
 		{
 			// Handle when your app resumes
 		}
--- XamarinFormSkiaSharp/XamarinFormSkiaSharp/MainPage.xaml.cs
+++ XamarinFormSkiaSharp/XamarinFormSkiaSharp/MainPage.xaml.cs
@@ -28,17 +28,38 @@ namespace XamarinFormSkiaSharp
 			if(contaienr == null) { return; }
 
 			var scale = (float)(e.Info.Width / contaienr.Width);
+#if DEBUG
+#if !WINDOWS_UWP
 			Console.WriteLine("Scale = {0}", scale);
+#else
+			System.Diagnostics.Debug.WriteLine("Scale = {0}", scale);
+#endif
+#endif
 			canvas.Scale(scale);
 
-			var circleFill = new SkiaSharp.SKPaint
-            {
-                IsAntialias = true,
-                Style = SKPaintStyle.Stroke,
-                Color = SKColors.Blue
-            };
 
-            canvas.DrawCircle(100, 100, 100, circleFill);
+			canvas.DrawCircle(100, 100, 100, new SKPaint
+			{
+				IsAntialias = true,
+				Style = SKPaintStyle.Stroke,
+				StrokeWidth = 1,
+				Color = SKColors.Blue
+			});
+
+			canvas.DrawCircle(100, 100, 80, new SKPaint
+			{
+				IsAntialias = true,
+				Style = SKPaintStyle.Stroke,
+				StrokeWidth = 3,
+				Color = SKColors.DarkRed
+			});
+
+			canvas.DrawCircle(100, 100, 50, new SKPaint
+			{
+				IsAntialias = true,
+				Style = SKPaintStyle.Fill,
+				Color = SKColors.DarkCyan
+			});
 
         }
     }
